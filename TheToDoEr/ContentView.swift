8b8895d39cd0fb3b6159d668fed37981518f0374
Magnus <51696488+Magnusahl@users.noreply.github.com>
2020-11-25 09:25:59 +0100

    Initial Commit

---
+++ TheToDoEr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheToDoEr
+//
+//  Created by Magnus Ahlqvist on 2020-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheToDoEr/TheToDoErApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheToDoErApp.swift
+//  TheToDoEr
+//
+//  Created by Magnus Ahlqvist on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct TheToDoErApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
